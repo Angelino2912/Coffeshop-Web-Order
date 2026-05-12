075e81c86d11c159a1e3c73f3f82bfdd71a14fe0
zakialfarisi1234-hue <zakialfarisi1234@gmail.com>
2026-05-12 18:06:38 +0700

    lanjutan yang sebelumnya ada kurang sedikit

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Order;
-
+use Illuminate\Http\Request;
 class AdminController extends Controller
 {
     public function orders()
@@ -18,4 +18,19 @@ class AdminController extends Controller
     {
         return view('admin.dashboard');
     }
+
+    public function updateStatus(Request $request, $id)
+    {
+        $order = Order::findOrFail($id);
+
+        $order->status = $request->status;
+
+        $order->save();
+
+        return back()->with(
+            'success',
+            'Status pesanan berhasil diupdate'
+        );
+    }
+
 }
